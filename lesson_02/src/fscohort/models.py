4644528fe9cbea556f13ad353118932e0fb2ac24
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-25 00:03:25 +0300

    Update models.py

--- lesson_02/src/fscohort/models.py
+++ lesson_02/src/fscohort/models.py
@@ -7,3 +7,6 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
+
+    def __str__(self):
+      return self.last_name
