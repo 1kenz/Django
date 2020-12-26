2aa3ce0694a848719eee5cb75f9e8678f8a340d8
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-26 19:44:57 +0300

    Update forms.py

--- in-class/lesson_03/src/fscohort/forms.py
+++ in-class/lesson_03/src/fscohort/forms.py
@@ -7,7 +7,20 @@ from .models import Student
 #     number = forms.IntegerField()
 
 
+# class StudentForm(forms.ModelForm):
+#     class Meta:
+#         model = Student
+#         fields = ("first_name", "last_name")
+
+
 class StudentForm(forms.ModelForm):
+    first_name = forms.CharField (label="Your Name")  # override
+
     class Meta:
         model = Student
-        fields = ("first_name", "last_name")
+        fields = "__all__"
+
+    # override
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args,**kwargs)
+    #     self.fields["first_name"].label = "My name"
