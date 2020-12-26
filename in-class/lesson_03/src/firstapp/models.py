e86d545ae1a19cc53cd3f14059e5768d83b7bf6b
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-26 19:26:26 +0300

    feat: add django forms

--- in-class/lesson_03/src/firstapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.Model):
-  first_name = models.CharField(max_length=50)
-  last_name = models.CharField(max_length=75)
-  number = models.IntegerField()
