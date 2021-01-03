a75dd42947f81247b3ca348f2b9436cdc62d3065
xkendx <63884610+xkendx@users.noreply.github.com>
2021-01-03 22:19:16 +0300

    feat: update urls

--- review/lesson_02/src/fscohort/urls.py
+++ review/lesson_02/src/fscohort/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from . import views
-from .views import home_view
+from .views import about_view, home_view
 
 
 urlpatterns = [
     # path("home/",views.home_view)
-    path("home/",home_view)
+    path("", home_view),
+    path("about/", about_view)
 ]
 
 
