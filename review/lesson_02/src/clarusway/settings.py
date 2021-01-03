09725bdf133aa32d6e05ea43c97fc1a5403a27a2
xkendx <63884610+xkendx@users.noreply.github.com>
2021-01-03 21:50:23 +0300

    Update settings.py

--- review/lesson_02/src/clarusway/settings.py
+++ review/lesson_02/src/clarusway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^1dke1irqvt6jjz+m7)hus^@t9%5c=(87^625-_s78u!*8eh4b'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
