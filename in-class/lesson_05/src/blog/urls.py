d6cb4674499081c9c65da9019871542e02585fe3
xkendx <63884610+xkendx@users.noreply.github.com>
2021-01-05 22:20:36 +0300

    fix: urls.py

--- in-class/lesson_05/src/blog/urls.py
+++ in-class/lesson_05/src/blog/urls.py
@@ -5,6 +5,6 @@ from .views import post_list, post_create, post_detail
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name="list"),
-    path("create/", post_create, name="create")
+    path("create/", post_create, name="create"),
     path("<str:slug/", post_detail, name="detail")
 ]
